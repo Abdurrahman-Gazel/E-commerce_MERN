ba5be23e0f3e9180dcee39d7f7f6fc456c399451
Abdurrahman Gazel <abdurahmangazel@gmail.com>
2025-05-22 20:06:21 +0300

    checkout integration + order success page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,26 +8,28 @@ import CartPage from "./pages/CartPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 import CartProvider from "./context/Cart/CartProvider";
 import CheckoutPage from "./pages/CheckoutPage";
+import OrderSuccessPage from "./pages/OrderSuccessPage";
 
 function App() {
   return (
     <AuthProvider>
       <CartProvider>
-       <BrowserRouter>
-         <Navbar />
-         <Routes>
-           <Route path="/" element={<HomePage />} />
-           <Route path="/register" element={<RegisterPage />} />
-           <Route path="/login" element={<LoginPage />} />
-           <Route element={<ProtectedRoute />}>
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/checkout" element={<CheckoutPage />} />
-          </Route>
-         </Routes>
-       </BrowserRouter>
-       </CartProvider>
-     </AuthProvider>
-  )
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/checkout" element={<CheckoutPage />} />
+              <Route path="/order-success" element={<OrderSuccessPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
